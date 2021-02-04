d98d95e23269042bdc7858a6070b3e6b0dffa216
Shucks <shucks@web.com>
2021-02-04 11:20:59 +0100

    .

--- src/NimbleImGui.nim
+++ src/NimbleImGui.nim
@@ -21,6 +21,7 @@ proc init =
   assert igGlfwInitForOpenGL(GLFWWin, true)
   assert igOpenGL3Init()
   igGetIO().iniFilename = joinPath(splitFile(paramStr(0)).dir, "gui.ini")
+  echo igGetIO().iniFilename
   Modules = parseModules()
   Installed = parseInstalled()
 
